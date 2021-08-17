f82ebb6e40c9a80a13d7f31a76a0c1256b00684d
Adrian Sims <adriansims62781@gmail.com>
2021-08-17 09:37:19 +0200

    Add project files.

---
+++ LamCap1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+using System.Threading.Tasks;
+
+namespace LamCap1
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                var alist = new List<Action>();
+                for (int i = 0; i < 10; i++)
+                {
+                    Action<int> act = x => Console.WriteLine(x);
+                    alist.Add(GetAction(i, act));
+                }
+
+                alist[0].Invoke();
+                alist[1].Invoke();
+                Console.WriteLine();
+            }
+            catch (Exception ex)
+            {
+                var fullname = System.Reflection.Assembly.GetEntryAssembly().Location;
+                var progname = Path.GetFileNameWithoutExtension(fullname);
+                Console.Error.WriteLine($"{progname} Error: {ex.Message}");
+            }
+        }
+
+        private static Action GetAction(int i, Action<int> a)
+        {
+            Action act = () => a(i);
+            return act;
+        }
+    }
+}
